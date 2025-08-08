8fcc8af45b55a4d3e5682c2891892a852e5bc692
ermanno <beeki96@gmail.com>
2025-08-08 13:54:48 +0200

    Add files via upload

---
+++ task-2-author-template/themes/clickonemedia/single-post.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+    <div class="container">
+      <?php
+      the_title('<h1>', '</h1>');
+      the_content(); ?>
+    </div>
+<?php
+    endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
